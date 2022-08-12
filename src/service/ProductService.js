e64bcd755f05e7fd9f034f0ead19d12d2b2c1c0f
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-08-11 22:54:12 -0400

    changes done

--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -2,5 +2,7 @@ export class ProductService {
 
     getProduct() {
         return fetch('mockdata.json').then(res => res.json()).then(d => d.data);
+        return fetch('mockdatamen.json').then(res => res.json()).then(d => d.data);
+        return fetch('mockdatateengirl.json').then(res => res.json()).then(d => d.data);
     }
 }
