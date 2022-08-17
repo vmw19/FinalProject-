a0c1dac12a7714b9c52bce0bc55fbf03a756582f
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-08-16 22:46:01 -0400

    size options

---
+++ src/clothing/components/MensBottomSizeOption.jsx
@@ -0,0 +1,21 @@
+import { SelectButton } from 'primereact/selectbutton';
+import React, { useState } from 'react';
+
+const MensBottomSizeOption = () => {
+    const [value1, setValue1] = useState('1');
+    const shirtOptions = ['extra small','small', 'med', 'large', 'extra large', '1x', '2x', '3x', '4x'];
+
+    const justifyTemplate = (option) => {
+        return <i className={option.icon}></i>;
+    }
+
+    return (
+        <div>
+            <div className="card">
+                <h5>Mens Bottom Size Options</h5>
+                <SelectButton value={value1} options={shirtOptions} onChange={(e) => setValue1(e.value)} />
+            </div>
+        </div>
+    );
+}
+export default MensBottomSizeOption;
---
+++ src/clothing/components/MensShoeSizeOption.jsx
@@ -0,0 +1,21 @@
+import { SelectButton } from 'primereact/selectbutton';
+import React, { useState } from 'react';
+
+const MensShoeSizeOption = () => {
+    const [value1, setValue1] = useState('1');
+    const shirtOptions = ['6','6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12'];
+
+    const justifyTemplate = (option) => {
+        return <i className={option.icon}></i>;
+    }
+
+    return (
+        <div>
+            <div className="card">
+                <h5>Mens Shoe Size Options</h5>
+                <SelectButton value={value1} options={shirtOptions} onChange={(e) => setValue1(e.value)} />
+            </div>
+        </div>
+    );
+}
+export default MensShoeSizeOption;
