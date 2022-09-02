4f70cd078e7bec748052d741ded8fcf544241413
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-09-01 21:51:40 -0400

    teengirlshoesize

--- src/clothing/components/TeenGirlShoeSizeOption.jsx
+++ src/clothing/components/TeenGirlShoeSizeOption.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const TeenGirlShoeSizeOption = () => {
     const [value1, setValue1] = useState('1');
-    const shirtOptions = ['6','6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12'];
+    const shoeOptions = ['6','6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12'];
 
     const justifyTemplate = (option) => {
         return <i className={option.icon}></i>;
@@ -13,7 +13,7 @@ const TeenGirlShoeSizeOption = () => {
         <div>
             <div className="card">
                 <h5>Teen Girl Shoe Size Options</h5>
-                <SelectButton value={value1} options={shirtOptions} onChange={(e) => setValue1(e.value)} />
+                <SelectButton value={value1} options={shoeOptions} onChange={(e) => setValue1(e.value)} />
             </div>
         </div>
     );
